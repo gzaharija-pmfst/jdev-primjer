7d10c3cd436f4aa9b4da8b5ff7c7380f76165a81
gzaharija <gzaharija@pmfst.hr>
2023-04-10 19:18:45 +0200

    Primjer astronauti

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+import axios from "axios";
+import Astronaut from "./Astronaut";
 import './App.css'
-
+ 
 function App() {
-
+  const [ljudi, postaviLjude] = useState([]);
+ 
+  function dohvatiPodatke() {
+    axios
+      .get("http://api.open-notify.org/astros.json")
+      .then(res => postaviLjude(res.data.people))
+      .catch(err => alert(err));
+  }
+ 
   return (
-    <div className="App">
-      <h1>Uvod u git</h1>
-      <p>Praćenje promjena u udaljenom repozitoriju</p>
-      <p>Još jedan paragaf</p>
+    <div className='App'>
+      <h1>Astronauti u svemiru</h1>
+      <button onClick={dohvatiPodatke}>Dohvati podatke</button>
+      <div className="okvir">
+        {ljudi.map(el => (
+          <Astronaut key={el.name} name={el.name} craft={el.craft} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
+ 
+export default App;
